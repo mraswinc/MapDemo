b2b3faaa2fb86f1c68e7d27646a84b6c0b9e421c
mraswinc <aswinboy@outlook.com>
2016-01-28 03:50:06 +0800

    FirstCommit

---
+++ MapDemo/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MapDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(SplitViewCtrl.IsPaneOpen==true)
+            {
+                SplitViewCtrl.IsPaneOpen = false;
+            }
+            else
+            {
+                SplitViewCtrl.IsPaneOpen = true;
+            }
+        }
+
+        private void MapBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(Map));
+        }
+    }
+}
